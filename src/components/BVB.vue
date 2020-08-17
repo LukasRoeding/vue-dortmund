86b58994bd29a99bb6d61fd49c637cd8fe0e1278
Lukas <goskodovesche@gmail.com>
2020-08-17 13:27:45 +0200

    Update BVB.vue

--- src/components/BVB.vue
+++ src/components/BVB.vue
@@ -15,9 +15,23 @@ export default {
   name: 'bvb',
   data() {
     return{
-      api_key: '8e5ff17cdbccda50197ace99031c1c5f'
+      api_key: '8e5ff17cdbccda50197ace99031c1c5f',
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query:'dortmund',
+      weather:{}
     }
-  }
+  },
+  created(){
+    fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+      .then(res => {
+        return res.json();
+      }).then(this.setResults);
+  },
+  methods:{
+  setResults (results){
+    this.weather = results;
+    console.log(this.weather)
+  }}
 }
 </script>
 
